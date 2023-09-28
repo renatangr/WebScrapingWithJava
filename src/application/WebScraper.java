39acef2abfb3fa300a47cca19458a812a41bb481
Renata Negrini | Estudante de ADS | 4º Sem <renatanegrinim@gmail.com>
2023-09-28 17:57:17 -0300

    Comentários inseridos

--- src/application/WebScraper.java
+++ src/application/WebScraper.java
@@ -12,13 +12,18 @@ import org.jsoup.select.Elements;
 public class WebScraper {
 
     public static void main(String[] args) {
+        // Cria um objeto Scanner para ler a entrada do usuário
         Scanner scan = new Scanner(System.in);
         
+        // Solicita ao usuário que insira a URL do produto NETSHOES desejado
         System.out.println("Digite a URL do produto NETSHOES que deseja: ");
         String url = scan.nextLine();
+        
+        // Fecha o Scanner após a leitura da entrada do usuário    
         scan.close();
         
-        try {          
+        try {        
+        // Conecta-se à URL especificada usando Jsoup para realizar a raspagem de dados
             Document doc = Jsoup.connect(url) 
                     .ignoreContentType(true)
                     .followRedirects(true) 
@@ -27,38 +32,44 @@ public class WebScraper {
                     .execute() 
                     .parse();
 
-            Product marketplaceProduct = new Product();
+            // Cria um objeto Product para armazenar os dados do produto
+             Product marketplaceProduct = new Product();
 
+            // Extrai o nome do produto, o preço, descrição e imagem da página da web
             Elements productElement = doc.select(".short-description h1");
             Elements priceElement = doc.select(".price strong");
             Elements descriptionElement = doc.select(".features p");
             Elements imageElement = doc.select(".photo figure");
-
+            
+            // Atribui a informação raspada ao atributo nome do objeto produto
             for (Element product : productElement) {
                 String productName = product.text();
                 marketplaceProduct.setName(productName);
             }
-
+            
+            // Atribui a informação raspada ao atributo preço do objeto produto
             for (Element product : priceElement) {
                 String productPrice = product.text();
                 marketplaceProduct.setPrice(productPrice);
             }
-
+            
+            // Atribui a informação raspada ao atributo descrição do objeto produto
             for (Element product : descriptionElement) {
                 String productDescription = product.text();
                 marketplaceProduct.setDescription(productDescription);
             }
-
+            
+            // Atribui a informação raspada ao atributo imagem do objeto produto
             for (Element product : imageElement) {
                 marketplaceProduct.setImage(product.selectFirst("img").attr("src"));
             }
 
             try (FileWriter writer = new FileWriter("dadosDoProduto.txt")) {
-                
+                // Escreve os dados do produto no arquivo de saída
                 writer.write("Abaixo toda a raspagem de dados: ");
                 writer.append("\n-------------------------\n");
                 writer.append(marketplaceProduct.toString());
-                writer.append("\n(Feito com <3 por @renatangr | Usando Java + Jsoup lib)");
+                writer.append("\n\n Feito com <3 por @renatangr | Usando Java + Jsoup lib");
             
             } catch (IOException e) {
                 e.printStackTrace();
