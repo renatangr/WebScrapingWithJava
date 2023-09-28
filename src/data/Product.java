49cb0be75329c1d82d1d0e014a68eef2446b58af
Renata Negrini | Estudante de ADS | 4º Sem <renatanegrinim@gmail.com>
2023-09-28 17:27:29 -0300

    Getters and Setters Product.java

--- src/data/Product.java
+++ src/data/Product.java
@@ -5,4 +5,38 @@ public class Product {
     private String price;
     private String description;
     private String image; 
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
 }
