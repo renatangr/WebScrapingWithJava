1f6f4eca12af38e7d1ca96290a871326d822a990
Renata Negrini | Estudante de ADS | 4º Sem <renatanegrinim@gmail.com>
2023-09-28 17:31:36 -0300

    add import jsoup

--- src/application/WebScraper.java
+++ src/application/WebScraper.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package application;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-/**
- *
- * @author RenataMorinigoLimaNe
- */
 public class WebScraper {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
